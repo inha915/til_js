a9cceaafef367ac612464caefcab02d9561d9181
inha915 <dlsgk0915@gmail.com>
2025-05-14 13:20:55 +0900

    함수 클래스 메소드

--- js/lib.js
+++ js/lib.js
@@ -1,10 +1,12 @@
-const Person = {
-  name: "아이유",
-  age: 20,
-  sayHi: function () {
-    console.log(this);
-  },
-};
-Person.name;
-Person.age;
-Person.sayHi();
+const say = function () {};
+const cry = () => {};
+
+// 매개변수로 전달된 함수 실행
+function run(a) {
+  a();
+}
+
+run(say);
+run(cry);
+
+run(function () {});
