7c420ab115fb4343307c9a2b00fd0fb3a5404630
inha915 <dlsgk0915@gmail.com>
2025-05-16 13:20:31 +0900

    마지막 문법

--- js/lib.js
+++ js/lib.js
@@ -1,9 +1,36 @@
-class PersonInfo {
-  constructor(_name, _age) {
-    this.nickName = _name;
-    this.age = _age;
-    this.job = "singer";
-  }
+// 데이터 서버에 자료를 호출함.
+
+function getData(api = "posts") {
+  return new Promise(function (resolve, reject) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://jsonplaceholder.typicode.com/${api}`);
+    xhr.send();
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        // 성공
+        resolve(xhr.response);
+      } else if (xhr.status === 404) {
+        // 실패
+        reject();
+      } else if (xhr.status === 505) {
+        console.log("서버가 불안정합니다. 잠시 후 재접속해주세요.");
+      }
+    };
+  });
 }
-const 아이유 = new PersonInfo("아이유", 20);
-const BTS = new PersonInfo("BTS", 20);
+// 콜백함수 만들기 : 자료가 들어오면 처리함.
+const postsParser = function (res) {
+  console.log(res);
+};
+const commentsParser = function (res) {};
+const albumsParser = function (res) {};
+const photosParser = function (res) {};
+const todosParser = function (res) {};
+const usersParser = function (res) {};
+// 함수 사용
+getData("posts", postsParser);
+getData("comments", commentsParser);
+getData("albums", albumsParser);
+getData("photos", photosParser);
+getData("todos", todosParser);
+getData("users", usersParser);
